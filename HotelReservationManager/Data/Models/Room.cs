f18bee08d5abfddbe60723a743dafbc5cfee2475
Edge <ece_sherif@abv.bg>
2025-04-01 23:10:10 +0300

    Initial

---
+++ HotelReservationManager/Data/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationManager.Data.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public uint Capacity { get; set; }
+
+        public RoomType Type { get; set; }
+
+        public bool Free { get; set; }
+
+        [Display(Name = "Цена за възрастен")]
+        public double PriceAdult { get; set; }
+
+        [Display(Name = "Цена за дете")]
+        public double PriceKid { get; set; }
+
+        [StringLength(50)]
+        public string Number { get; set; }
+    }
+}
